ecc6396d72734e34a3bf48eaae91c97c16a60231
fikrim1109 <88815201+fikrim1109@users.noreply.github.com>
2023-11-09 18:23:07 +0800

    feat : tambahkan fitur ganti nama dan trofi

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,23 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import './globals.css'
+
 export default function Home() {
+
+  const [Dor, setDor] = useState(0)
+  const [nama, setNama] = useState('Fikri Natadiwirya Maulana')
+
+  function handlerTambahBravo(){
+    setDor(Dor + 1)
+  }
+
+  function handlerGantiNama(){
+    setNama('Lionel Messi')
+  }
+  
+  
   return (
     <div className='body'>
       <div className="banner-container">
@@ -18,22 +35,27 @@ export default function Home() {
           </div>
           <div className="content-header-banner">
             {/* Nama dan Kawan2*/}
-            <h1>Fikri Natadiwirya Maulana</h1>
+            <h1>{nama}</h1>
             <div className="bio-nim-header-banner">
             {/* NIM dan BIO*/}
             <p>D121211033</p>
-            <p>8 ballon D'or winners</p>
+            <p>{Dor} Balon D'or Winner</p>
             </div>
           </div>
         </div>
         <div className="cta-banner-wrapper">
           {/* Tombol CTA */}
-          <a href=''>
-            <div className='cta-button'>
-              <p>Halo!</p>
+            <div className='cta-button' onClick={handlerTambahBravo}>
+              <p>Tambah Trofi </p>
             </div>
-          </a>
-          
+            <div className='cta-button' 
+            style={{
+              marginTop: '12px'
+            }}
+            
+            onClick={handlerGantiNama}>
+              <p>Ganti Nama</p>
+            </div>
         </div>
       </div>
     </div>
