a7a1e22fb26e6882c46e94e5d2871417b7add50c
fikrim1109 <88815201+fikrim1109@users.noreply.github.com>
2023-11-11 17:33:03 +0800

    feat = add input field

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,23 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import Image from 'next/image'
-import './globals.css'
+import { useState } from "react";
+import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
+  const [nama, setNama] = useState("Fikri Natadiwirya Maulana");
+  const [newNama, setNewNama] = useState("");
 
-  const [Dor, setDor] = useState(0)
-  const [nama, setNama] = useState('Fikri Natadiwirya Maulana')
-
-  function handlerTambahDor(){
-    setDor(Dor + 1)
+  function handlerGantiNama() {
+    setNama(newNama);
   }
 
-  function handlerGantiNama(){
-    setNama('Lionel Messi')
+  function handleInputChange(event) {
+    setNewNama(event.target.value);
   }
-  
-  
+
   return (
-    <div className='body'>
+    <div className="body">
       <div className="banner-container">
         {/* Kartunya */}
         <div className="header-banner-wrapper">
@@ -30,34 +28,35 @@ export default function Home() {
               src="/assets/fotomessi.png"
               alt="Picture of the author"
               fill
-              objectFit='contain'
+              objectFit="contain"
             />
           </div>
           <div className="content-header-banner">
             {/* Nama dan Kawan2*/}
             <h1>{nama}</h1>
             <div className="bio-nim-header-banner">
-            {/* NIM dan BIO*/}
-            <p>D121211033</p>
-            <p>{Dor} Balon D'or Winner</p>
+              {/* NIM dan BIO*/}
+              <p>D121211033</p>
+              <p>Messi punya 8 Ballon D'or</p>
             </div>
           </div>
         </div>
         <div className="cta-banner-wrapper">
           {/* Tombol CTA */}
-            <div className='cta-button' onClick={handlerTambahDor}>
-              <p>Tambah Trofi </p>
-            </div>
-            <div className='cta-button' 
+          <div>
+            <input type="text" value={newNama} onChange={handleInputChange} />
+          </div>
+          <div
+            className="cta-button"
             style={{
-              marginTop: '12px'
+              marginTop: "12px",
             }}
-            
-            onClick={handlerGantiNama}>
-              <p>Ganti Nama</p>
-            </div>
+            onClick={handlerGantiNama}
+          >
+            <p>Ganti Nama</p>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
